500289bd547a11689628f058b070a4e46fe2029f
adrian <adrian@blinkenlights.ch>
2004-05-01 15:14:56 +0000

    Tool for .m4a files (Read tags)

---
+++ tools/m4a-info.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use GNUpod::QTfile;
+
+die "Usage: $0 m4a-file\n" unless @ARGV;
+
+foreach(@ARGV) {
+  my $fref = GNUpod::QTfile::parsefile($_);
+  
+  print "\nFile '$_'\n";
+  
+  if(ref($fref) eq "HASH") {
+   foreach(keys(%$fref)) {
+    printf ("  %-12s :  %s\n",$_,$fref->{$_});
+   }
+  }
+  else {
+   print "  [!!] Not an M4A File\n";
+  }
+}
