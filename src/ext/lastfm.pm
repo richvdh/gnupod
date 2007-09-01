6312fb77ff151777be2c5bb6f358f141fde6b908
adrian <adrian@blinkenlights.ch>
2007-09-01 19:13:01 +0000

    Upgraded to GPLv3

--- src/ext/lastfm.pm
+++ src/ext/lastfm.pm
@@ -1,5 +1,24 @@
 package GNUpod::lastfm;
 
+#  Copyright (C) 2006-2007 Adrian Ulrich <pab at blinkenlights.ch>
+#  Part of the gnupod-tools collection
+#
+#  URL: http://www.gnu.org/software/gnupod/
+#
+#    GNUpod is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GNUpod is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.#
+#
+
 use strict;
 use Digest::MD5;
 use LWP::UserAgent;
